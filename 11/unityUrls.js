a9142294d2deea55c234706d4566c3e40d6537b2
bobydob <80217817+bobydob@users.noreply.github.com>
2023-11-15 01:46:50 +0300

    Add files via upload

---
+++ 11/unityUrls.js
@@ -0,0 +1,10 @@
+window.loadedUrls = true;
+
+function getBuildUrl() {
+	return "https://justbuild.nyc3.cdn.digitaloceanspaces.com/NewCI/1v1/Prod/182/eb79ce73084a43d888f3cafc8475a5d4";
+}
+
+function getGameLoaderUrl() {
+	return getBuildUrl() + "/WebGL.loader.js";
+}
+
