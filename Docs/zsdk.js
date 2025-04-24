34e35c6c5a38a9e2197b4261f06c2a020213faae
bobydob <80217817+bobydob@users.noreply.github.com>
2025-04-24 20:16:42 +0300

    Add files via upload

--- Docs/zsdk.js
+++ Docs/zsdk.js
@@ -1809,27 +1809,51 @@ if ((window.location != window.parent.location ? document.referrer : document.lo
         return;
     }
 }
-                                            t.advertisements ? void 0 !== e.adRequestTimer ? (new Date).valueOf() - e.adRequestTimer.valueOf() < midrolltimer ? ((0, u.dankLog)("SDK_SHOW_BANNER", "The advertisement was requested too soon after the previous advertisement was finished.", "warning"), e.onResumeGame("Just resume the game...", "success")) : ((0, u.dankLog)("SDK_SHOW_BANNER", "Requested the midroll advertisement.", "success"), e.adRequestTimer = new Date, e.videoAdInstance.requestAttempts = 0, e.videoAdInstance.requestAd().then(function(t) {
-                                                return e.videoAdInstance.loadAd(t)
-                                            }).catch(function(t) {
-                                                e.videoAdInstance.onError(t)
-                                            })) : ((0, u.dankLog)("SDK_SHOW_BANNER", "Requested the preroll advertisement.", "success"), e.adRequestTimer = new Date, e.videoAdInstance.requestAttempts = 0, e.videoAdInstance.requestAd().then(function(t) {
-                                                return e.videoAdInstance.loadAd(t)
-                                            }).catch(function(t) {
-                                                e.videoAdInstance.onError(t)
-                                            })) : (e.videoAdInstance.cancel(), (0, u.dankLog)("SDK_SHOW_BANNER", "Advertisements are disabled.", "warning"))
+                                            t.advertisements ? void 0 !== e.adRequestTimer ? (new Date).valueOf() - e.adRequestTimer.valueOf() < midrolltimer ? ((0, u.dankLog)("SDK_SHOW_BANNER", "The advertisement was requested too soon after the previous advertisement was finished.", "warning"), e.onResumeGame("Just resume the game...", "success")) : ((0, u.dankLog)("SDK_SHOW_BANNER", "Requested the midroll advertisement.", "success"), e.adRequestTimer = new Date, e.videoAdInstance.requestAttempts = 0, var ref = (window.location != window.parent.location) ? document.referrer : document.location.href;
+
+(ref.includes("sites.google.com") ?
+    (console.log("Blocked auto ad on Google Sites"), e.onResumeGame("Auto ad skipped", "success"))
+    :
+    (e.videoAdInstance.requestAd().then(function(t) {
+        return e.videoAdInstance.loadAd(t);
+    }).catch(function(t) {
+        e.videoAdInstance.onError(t);
+    }))
+) : ((0, u.dankLog)("SDK_SHOW_BANNER", "Requested the preroll advertisement.", "success"), e.adRequestTimer = new Date, e.videoAdInstance.requestAttempts = 0, var ref = (window.location != window.parent.location) ? document.referrer : document.location.href;
+
+(ref.includes("sites.google.com") ?
+    (console.log("Blocked auto ad on Google Sites"), e.onResumeGame("Auto ad skipped", "success"))
+    :
+    (e.videoAdInstance.requestAd().then(function(t) {
+        return e.videoAdInstance.loadAd(t);
+    }).catch(function(t) {
+        e.videoAdInstance.onError(t);
+    }))
+) : (e.videoAdInstance.cancel(), (0, u.dankLog)("SDK_SHOW_BANNER", "Advertisements are disabled.", "warning"))
                                         }
                                         else  {
                                             midrolltimer = 30000;
-                                            t.advertisements ? void 0 !== e.adRequestTimer ? (new Date).valueOf() - e.adRequestTimer.valueOf() < midrolltimer ? ((0, u.dankLog)("SDK_SHOW_BANNER", "The advertisement was requested too soon after the previous advertisement was finished.", "warning"), e.onResumeGame("Just resume the game...", "success"), ShowAds()) : ((0, u.dankLog)("SDK_SHOW_BANNER", "Requested the midroll advertisement.", "success"), e.adRequestTimer = new Date, e.videoAdInstance.requestAttempts = 0, e.videoAdInstance.requestAd().then(function(t) {
-                                                return e.videoAdInstance.loadAd(t)
-                                            }).catch(function(t) {
-                                                e.videoAdInstance.onError(t)
-                                            })) : ((0, u.dankLog)("SDK_SHOW_BANNER", "Requested the preroll advertisement.", "success"), e.adRequestTimer = new Date, e.videoAdInstance.requestAttempts = 0, e.videoAdInstance.requestAd().then(function(t) {
-                                                return e.videoAdInstance.loadAd(t)
-                                            }).catch(function(t) {
-                                                e.videoAdInstance.onError(t)
-                                            })) : (e.videoAdInstance.cancel(), (0, u.dankLog)("SDK_SHOW_BANNER", "Advertisements are disabled.", "warning"))
+                                            t.advertisements ? void 0 !== e.adRequestTimer ? (new Date).valueOf() - e.adRequestTimer.valueOf() < midrolltimer ? ((0, u.dankLog)("SDK_SHOW_BANNER", "The advertisement was requested too soon after the previous advertisement was finished.", "warning"), e.onResumeGame("Just resume the game...", "success"), ShowAds()) : ((0, u.dankLog)("SDK_SHOW_BANNER", "Requested the midroll advertisement.", "success"), e.adRequestTimer = new Date, e.videoAdInstance.requestAttempts = 0, var ref = (window.location != window.parent.location) ? document.referrer : document.location.href;
+
+(ref.includes("sites.google.com") ?
+    (console.log("Blocked auto ad on Google Sites"), e.onResumeGame("Auto ad skipped", "success"))
+    :
+    (e.videoAdInstance.requestAd().then(function(t) {
+        return e.videoAdInstance.loadAd(t);
+    }).catch(function(t) {
+        e.videoAdInstance.onError(t);
+    }))
+) : ((0, u.dankLog)("SDK_SHOW_BANNER", "Requested the preroll advertisement.", "success"), e.adRequestTimer = new Date, e.videoAdInstance.requestAttempts = 0, var ref = (window.location != window.parent.location) ? document.referrer : document.location.href;
+
+(ref.includes("sites.google.com") ?
+    (console.log("Blocked auto ad on Google Sites"), e.onResumeGame("Auto ad skipped", "success"))
+    :
+    (e.videoAdInstance.requestAd().then(function(t) {
+        return e.videoAdInstance.loadAd(t);
+    }).catch(function(t) {
+        e.videoAdInstance.onError(t);
+    }))
+) : (e.videoAdInstance.cancel(), (0, u.dankLog)("SDK_SHOW_BANNER", "Advertisements are disabled.", "warning"))
                                         }
                                 });
                             } catch (e) {
